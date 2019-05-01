5f9768aa666ce59f1e7e9397d89b3dd097e3ba96
Brennan <brennanduffey@gmail.com>
2019-05-01 15:11:50 -0600

    Fetchfilms method added to apiCalls.js

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,3 +1,10 @@
 export const fetchFilms = () => {
-  return 
+  return fetch("https://swapi.co/api/films/")
+  .then(response => {
+    if (!response.ok) {
+      throw Error('Error in fetching films');
+    } else {
+      return response.json();
+    }
+  })
 }
