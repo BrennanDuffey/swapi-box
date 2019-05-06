1d21f3a11146dcb7aeedd65963a611a4861e5dbd
Brennan <brennanduffey@gmail.com>
2019-05-05 22:44:40 -0600

    Vehicles fetched

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
 const Card = (props) => {
+  const selectCardType = (props) => {
+    let planetName;
+    let population;
+    let species;
+    if (props.selected === "people") {
+
+    }
+  }
   return (
     <article>
+      <h3>{props.name}</h3>
     </article>
   )
 }
